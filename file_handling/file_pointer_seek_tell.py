2f16b469a7592f98f2033770bc6f5cdb0f5aa4e7
Anas Hasan <anas.hassan9417@gmail.com>
2026-02-25 02:06:00 +0530

    Add file pointer operations in python

---
+++ file_handling/file_pointer_seek_tell.py
@@ -0,0 +1,12 @@
+with open('big.txt', 'r') as f:
+  print(f.read(10))
+  print(f.tell())
+  f.seek(15)
+  print(f.read(10))
+
+
+# seek during write
+with open('sample.txt', 'w') as f:
+  f.write('Hello')
+  f.seek(0)
+  f.write('X')
